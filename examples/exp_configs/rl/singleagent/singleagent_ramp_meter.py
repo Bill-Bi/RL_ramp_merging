8ad40ac54a7fff59667362bd8f4774097a168d0b
Xiaowei Bi <bill.bi.contractor@bedrockocean.com>
2021-12-02 13:06:38 -0600

    updated the inflow rate

--- examples/exp_configs/rl/singleagent/singleagent_ramp_meter.py
+++ examples/exp_configs/rl/singleagent/singleagent_ramp_meter.py
@@ -76,15 +76,15 @@ inflow = InFlows()
 inflow.add(
     veh_type="human",
     edge="inflow_highway",
-    # vehs_per_hour=(1 - RL_PENETRATION) * FLOW_RATE,
-    probability = (1 - RL_PENETRATION) * FLOW_PROBABILITY,
+    vehs_per_hour=(1 - RL_PENETRATION) * FLOW_RATE,
+    # probability = (1 - RL_PENETRATION) * FLOW_PROBABILITY,
     departLane="free",
     departSpeed=10)
 inflow.add(
     veh_type="rl",
     edge="inflow_highway",
-    # vehs_per_hour=RL_PENETRATION * FLOW_RATE,
-    probability = RL_PENETRATION * FLOW_PROBABILITY,
+    vehs_per_hour=RL_PENETRATION * FLOW_RATE,
+    # probability = RL_PENETRATION * FLOW_PROBABILITY,
     departLane="free",
     departSpeed=10)
 # inflow.add(
@@ -97,7 +97,7 @@ inflow.add(
     veh_type="idm",
     edge="inflow_merge",
     # vehs_per_hour=100,
-    probability = 100 / 3600,
+    probability = 300 / 3600,
     departLane="free",
     departSpeed=7.5)
 
